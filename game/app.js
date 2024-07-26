b738db4af55388f05f612d1e3b375b97c3674a12
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2024-07-26 15:03:31 -0300

    aula de JS

---
+++ game/app.js
@@ -0,0 +1,26 @@
+// AULA ACOMPANHADA
+
+// alert('boas-vindas ao jogo do numero secreto');
+// let numeroSecreto = 5;
+// let chute = prompt('Escolha um numero entre 1 e 30')
+// if(chute == numeroSecreto){
+//     alert(`Isso aê, você acertou o numero secreto: ${numeroSecreto}`)
+// } else{
+//     alert('Que pena você errou, tente novamente')
+// }
+
+// TAREFA DE CASA 
+
+alert('Boas-Vindas ao Nosso Site!');
+let nome = "lua";
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let idade = prompt('Digite sua idade:');
+
+if (idade >= 18) {
+alert('Pode tirar a habilitação!');
+}
+
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = "Erro! Preencha todos os campos!";
+alert(`${mensagemDeErro}`);
