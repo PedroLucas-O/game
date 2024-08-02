3d2e41a7a78cd584d38337254e230465302a19d2
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2024-08-02 16:42:28 -0300

    aula3

--- game/app.js
+++ game/app.js
@@ -1,29 +1,66 @@
 // AULA ACOMPANHADA
 
-alert('boas-vindas ao jogo do numero secreto');
+// alert('boas-vindas ao jogo do numero secreto')// let numeroSecreto = parseInt (Math.random()*numeroMaxim;
+// let numeroSecreto = gerarNumero();
 let numeroSecreto = 5;
-let chute = prompt('Escolha um numero entre 1 e 30')
-let tentativa=1
 
-while (chute != numeroSecreto) {
+// let numeroMaximo = 500;
+// let tentativa=1;
 
-     chute = prompt('Escolha um numero entre 1 e 30')
-    if(chute == numeroSecreto){
-    } else{
-    if(numeroSecreto > chute){
-        alert(`Numero Secreto é Maior que ${chute}`)
-    } else{
-        alert(`Numero Secreto é Menor que ${chute}`)
-   }
-   tentativa++
+
+// while (chute != numeroSecreto) {
+
+//      chute = prompt(`Escolha um numero entre 0 a ${numeroMaximo}`);
+//     if(chute == numeroSecreto){
+//         break;
+//     } else{
+//     if(numeroSecreto > chute){
+//         alert(`Numero Secreto é Maior que ${chute}`);
+//     } else{
+//         alert(`Numero Secreto é Menor que ${chute}`);
+//    }
+//    tentativa++;
+// }
+
+// }
+
+// let palavraTentativa = tentativa>1?"tentativas":"tentativa";
+// alert(`Isso aê Você Descobriu o Numero Secreto que é: ${numeroSecreto} com ${tentativa} ${palavraTentativa}`);
+
+// CODIGO BASE
+
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML='Jogo do Numero Secreto';
+
+// let tagP = document.querySelector('p');
+// tagP.innerHTML='Escolha um Numero Entre 1 e 10 '
+
+
+function verificarChute () {
+
+    let chute = document.querySelector('input').value
+    console.log(chute==numeroSecreto);
 }
 
+function exibirText (tag,texto) {
+
+    let titulo = document.querySelector(tag);
+    titulo.innerHTML = texto;
+
+}       
+
+exibirText('h1','Chalenge Time!');
+exibirText('p','Escolha Um Numero Entre 1 e 10')
+
+function gerarNumero () {
+    return parseInt (Math.random()*10+1);
+};
+gerarNumero();
+
+function mentiraPura () {
+    
+    alert ("EU AMO JAVASCRIPT DIMAISE");
+
 }
-
-let palavraTentativa = tentativa>1?"tentativas":"tentativa";
-alert(`Isso aê Você Descobriu o Numero Secreto que é: ${numeroSecreto} com ${tentativa} ${palavraTentativa}`)
-
-
-
 
 
